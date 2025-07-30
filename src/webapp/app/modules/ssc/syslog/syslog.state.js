ade639494a3457a2956f6f312912de4b6a5b3d9a
Jason <jason88.guo@gmail.com>
2025-07-30 16:41:52 +0800

    移除系统设置/系统日志菜单

--- src/webapp/app/modules/ssc/syslog/syslog.state.js
+++ src/webapp/app/modules/ssc/syslog/syslog.state.js
@@ -2,17 +2,7 @@
     'use strict';
     angular.module('oplus.app').config(['$stateProvider', function ($stateProvider) {
         $stateProvider
-            .state('app.ssc.config.syslog', {
-                url: '/syslog',
-                views: {
-                    'ssc_config': {
-                        templateUrl: function () {
-                            return 'app/modules/ssc/syslog/syslog.html';
-                        },
-                        controller: 'udpSyslogCtrl'
-                    }
-                }
-            })
+
         ;
     }]);
 })();
