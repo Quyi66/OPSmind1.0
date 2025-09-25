10f854892d5614c17c6149df210a0f35b7f363cc
Jason <jason88.guo@gmail.com>
2025-09-25 09:33:51 +0800

    add cac template/team draft version

--- src/webapp/app/modules/ssc/team/team-dialog.controller.js
+++ src/webapp/app/modules/ssc/team/team-dialog.controller.js
@@ -5,14 +5,17 @@
         .module('oplus.ssc')
         .controller('TeamDialogController', TeamDialogController);
 
-    TeamDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Team', 'currentUser', 'messageService'];
+    TeamDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Team', 'currentUser', 'messageService', 'cacTemplateService'];
 
-    function TeamDialogController($timeout, $scope, $stateParams, $uibModalInstance, entity, Team, currentUser, messageService) {
+    function TeamDialogController($timeout, $scope, $stateParams, $uibModalInstance, entity, Team, currentUser, messageService, cacTemplateService) {
         var vm = this;
         vm.team = entity;
         vm.clear = clear;
         vm.save = save;
         vm.users = [];
+        // CAC templates
+        vm.cacTemplates = [];
+        vm.selectedTemplateIds = [];
         var selectedUserMap = {};
 
         $timeout(function () {
@@ -23,6 +26,7 @@
 
         function init() {
             initUsers();
+            initTemplates();
             var selectedUsers = entity.users;
             for (var i in selectedUsers) {
                 var user = selectedUsers[i];
@@ -53,6 +57,35 @@
             });
         }
 
+        function initTemplates() {
+            cacTemplateService.getTemplates().then(function (templates) {
+                vm.cacTemplates = templates || [];
+                if (vm.team && vm.team.cacTemplateIds) {
+                    if (angular.isArray(vm.team.cacTemplateIds)) {
+                        vm.selectedTemplateIds = normalizeTemplateIdType(vm.team.cacTemplateIds);
+                    } else if (angular.isString(vm.team.cacTemplateIds)) {
+                        // support comma-separated values
+                        vm.selectedTemplateIds = normalizeTemplateIdType(vm.team.cacTemplateIds.split(','));
+                    }
+                }
+            }).catch(function () {
+                // Ignore template load error in team dialog; keep UI functional
+                vm.cacTemplates = [];
+            });
+        }
+
+        function normalizeTemplateIdType(idList) {
+            // Ensure the model type matches the option value type
+            var numeric = vm.cacTemplates.length > 0 && angular.isNumber(vm.cacTemplates[0].id);
+            if (!numeric) return idList;
+            var arr = [];
+            for (var i = 0; i < idList.length; i++) {
+                var n = parseInt(idList[i]);
+                if (!isNaN(n)) arr.push(n);
+            }
+            return arr;
+        }
+
 
         function clear() {
             $uibModalInstance.dismiss('cancel');
@@ -67,6 +100,8 @@
                 }
             }
             vm.team.users = selectedUsers;
+            // attach selected template ids to team payload
+            vm.team.cacTemplateIds = angular.copy(vm.selectedTemplateIds || []);
             Team.saveTeam(vm.team).then(function (result) {
                 onSaveSuccess(result);
             }).catch(function (err) {
