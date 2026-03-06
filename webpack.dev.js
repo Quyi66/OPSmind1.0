4efed3452c34b0933a6db990716b2880cf7fa439
yumiao <15384200337@qq.com>
2026-03-06 09:36:08 +0800

    fix:修改https相关信息

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ const webpack = require("webpack");
 
 module.exports = (env, argv) => {
   // 代理服务器配置
-  const PROXY_TARGET = "http://192.168.1.230";
+  const PROXY_TARGET = process.env.PROXY_TARGET || "https://192.168.1.230";
   const commonConfig = common(env, { ...argv, mode: "development" });
 
   return merge(commonConfig, {
